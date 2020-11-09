9c83a74bb51fbea307170b0fdf7d7d1fc55fd12f
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-11-09 16:18:07 +0100

    Fix tag to DQCsim repository

--- cmake/dqcsim.cmake
+++ cmake/dqcsim.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 FetchContent_Declare(dqcsim
 	GIT_REPOSITORY https://github.com/QE-Lab/dqcsim.git
-	GIT_TAG 0.2.4)
+	GIT_TAG master)
 set(DQCSIM_FROM_SOURCE "yes")
 FetchContent_MakeAvailable(dqcsim)
