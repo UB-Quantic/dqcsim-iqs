f9adeef3610d1f7ffa8686d59c1b1abc6f41c653
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-11-11 19:05:43 +0100

    Update dqcsim to 0.2.5
    Fixed CMake build from souce

--- cmake/dqcsim.cmake
+++ cmake/dqcsim.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 FetchContent_Declare(dqcsim
 	GIT_REPOSITORY https://github.com/QE-Lab/dqcsim.git
-	GIT_TAG master)
+	GIT_TAG 0.2.5)
 set(DQCSIM_FROM_SOURCE "yes")
 FetchContent_MakeAvailable(dqcsim)
