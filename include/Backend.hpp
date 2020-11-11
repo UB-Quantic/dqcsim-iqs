ceeb992b47723b9fe6d58f6f11cf1875eba058e8
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-11-11 19:49:06 +0100

    Add Backend interface

---
+++ include/Backend.hpp
@@ -0,0 +1,19 @@
+#define DQCSIM_SHORT_LOGGING_MACROS
+#include <dqcsim>
+#include <qureg.hpp>
+
+using namespace dqcsim::wrap;
+
+class IqsBackend {
+    QubitRegister<ComplexSP> reg;
+
+  public:
+    IqsBackend();
+    void initialize(PluginState &state, ArbCmdQueue &&queue);
+    void drop(PluginState &state);
+    void allocate(PluginState &state, QubitSet &&qubits, ArbCmdQueue &&queue);
+    void free(PluginState &state, QubitSet &&qubits);
+    MeasurementSet gate(PluginState &state, Gate &&arg);
+    void advance(PluginState &state, Cycle cycle);
+    ArbData arb_cmd(PluginState &state, ArbCmd cmd);
+};
