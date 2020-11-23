f3d4891e6bf7d9cfb403221fdc8e8007e3c2f385
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-11-23 17:01:06 +0100

    Update dqcsim dependency to 0.3.0
    Now it is imported using find_package

--- cmake/dqcsim.cmake
+++
@@ -1,6 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(dqcsim
-	GIT_REPOSITORY https://github.com/QE-Lab/dqcsim.git
-	GIT_TAG 0.2.5)
-set(DQCSIM_FROM_SOURCE "yes")
-FetchContent_MakeAvailable(dqcsim)
